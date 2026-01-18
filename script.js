3c259c7fbcab1ac038a892140695f057dd83b22f
Eternal Lecher <priyansh413sharma@gmail.com>
2026-01-18 18:55:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+Apk=gsap.timeline();
+Apk.from(".a",{
+    opacity:0,
+    y:-20,
+    duration:0.8,
+})
+Apk.from(".b",{
+    opacity:0,
+    y:-20,
+    duration:1,
+})
+Apk.from(".c",{
+     opacity:0,
+    y:-20,
+    duration:0.5,
+})
+function greet(){
+    alert("Appointment Send")
+}
+
